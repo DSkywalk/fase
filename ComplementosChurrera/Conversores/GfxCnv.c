92a175ce538d67138f218db7310562bc04aed160
David Skywalker <dantoine@gmail.com>
2014-06-03 23:01:58 +0200

    fix gfxcnv again :_

--- ComplementosChurrera/Conversores/GfxCnv.c
+++ ComplementosChurrera/Conversores/GfxCnv.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "lodepng.c"
+#include "lodepng.h"
 unsigned char *image, *pixel, output[0x600];
 unsigned error, width, height, i, j, k, l, fondo, tinta, outpos= 0;
 long long atr, celdas[4];
