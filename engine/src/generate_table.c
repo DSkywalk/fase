034872b436e4acf734be25e4add6527b296fee5c
antoniovillena <espineter@yahoo.com>
2015-05-21 23:56:33 +0200

    Cambio file1.bin

--- engine/src/generate_table.c
+++ engine/src/generate_table.c
@@ -2,7 +2,7 @@
 int main(int argc, char *argv[]){
   FILE *fo;
   unsigned char table[0x100];
-  fo= fopen("file1.bin", "wb+");
+  fo= fopen("../asm/file1.bin", "wb+");
   for ( int i= 0; i<0x100; i++ )
     table[i]=  i&0x07 | i>>3&0x18 | i<<2&0xe0;
   fwrite(table, 1, 0x100, fo);
